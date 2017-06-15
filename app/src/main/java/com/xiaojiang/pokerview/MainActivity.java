1f4e69440d8a6ebdb68384ef13821789d4ead4ba
guoxiaojiang <xiaojiang163@163.com>
2017-06-15 16:30:48 +0800

    增加点击翻页查看详情支持

--- app/src/main/java/com/xiaojiang/pokerview/MainActivity.java
+++ app/src/main/java/com/xiaojiang/pokerview/MainActivity.java
@@ -1,11 +1,12 @@
 package com.xiaojiang.pokerview;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import com.xiaojiang.pokerview.adapter.CommonAdapter;
 
@@ -17,12 +18,17 @@ public class MainActivity extends AppCompatActivity {
     CommonAdapter<PokerCardBean> mAdapter;
     protected List mDatas;
     PokerLayoutManager mPokerLayoutManager;
+    private AnimatorSet mLeftInSet, mRightOutSet, mLeftOutSet, mRightInSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mPokerView = (PokerView) findViewById(R.id.poker_view);
+        mRightOutSet = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.poker_item_card_anim_right_out);
+        mLeftInSet = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.poker_item_card_anim_left_in);
+        mLeftOutSet = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.poker_item_card_anim_left_out);
+        mRightInSet = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.poker_item_card_anim_right_in);
         mPokerView.setAdapter(mAdapter = new CommonAdapter<PokerCardBean>(this, mDatas = PokerCardBean.initDatas(), R.layout.item_poker_card) {
 
             @Override
@@ -35,6 +41,13 @@ public class MainActivity extends AppCompatActivity {
                 viewHolder.setText(R.id.tvName, pokerCardBean.getName());
                 viewHolder.setText(R.id.tvPrecent, pokerCardBean.getPostition() + " /" + mDatas.size());
                 viewHolder.setImageResource(R.id.imgContent, pokerCardBean.imgResource);
+                viewHolder.setText(R.id.txt_item_poker_card_detail, pokerCardBean.detail);
+                viewHolder.setOnClickListener(R.id.ll_item_poker_card_front_cover, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        flipCard(viewHolder.getView(R.id.ll_item_poker_card_front_cover), viewHolder.getView(R.id.ll_item_poker_card_detail));
+                    }
+                });
             }
 
             @Override
@@ -42,8 +55,26 @@ public class MainActivity extends AppCompatActivity {
                 super.onBindViewHolder(holder, position);
             }
         });
-        mPokerView.scrollToPosition(3);
+        mPokerView.scrollToPosition(0);
         mAdapter.notifyDataSetChanged();
+    }
+
+
+    // 翻转卡片
+    private void flipCard(View cardFront, View cardBack) {
+        if (cardFront.getAlpha() > 0) {
+            // 当前正面朝上
+            mRightOutSet.setTarget(cardFront);
+            mLeftInSet.setTarget(cardBack);
+            mRightOutSet.start();
+            mLeftInSet.start();
+        } else {
+            // 当前背面朝上
+            mLeftOutSet.setTarget(cardBack);
+            mRightInSet.setTarget(cardFront);
+            mLeftOutSet.start();
+            mRightInSet.start();
+        }
     }
 
     @Override
--- app/src/main/java/com/xiaojiang/pokerview/PokerCardBean.java
+++ app/src/main/java/com/xiaojiang/pokerview/PokerCardBean.java
@@ -8,12 +8,14 @@ public class PokerCardBean {
     private String url;
     private String name;
     public int imgResource;
+    public String detail;
 
-    public PokerCardBean(int postition, String url, String name, int imgResource) {
+    public PokerCardBean(int postition, String url, String name, int imgResource, String detail) {
         this.postition = postition;
         this.url = url;
         this.name = name;
         this.imgResource = imgResource;
+        this.detail = detail;
     }
 
     public int getPostition() {
@@ -46,14 +48,21 @@ public class PokerCardBean {
     public static List<PokerCardBean> initDatas() {
         List<PokerCardBean> datas = new ArrayList<>();
         int i = 1;
-        datas.add(new PokerCardBean(i++, null, "这是图片1", R.drawable.tu1));
-        datas.add(new PokerCardBean(i++, null, "这是图片2", R.drawable.tu2));
-        datas.add(new PokerCardBean(i++, null, "这是图片3", R.drawable.tu3));
-        datas.add(new PokerCardBean(i++, null, "这是图片4", R.drawable.tu4));
-        datas.add(new PokerCardBean(i++, null, "这是图片5", R.drawable.tu5));
-        datas.add(new PokerCardBean(i++, null, "这是图片6", R.drawable.tu6));
-        datas.add(new PokerCardBean(i++, null, "这是图片7", R.drawable.tu7));
-        datas.add(new PokerCardBean(i++, null, "这是图片8", R.drawable.tu8));
+        datas.add(new PokerCardBean(i++, null, "这是图片1", R.drawable.tu1, "习近平指出，中卢友好交往历史悠久。建交45年来，双方坚持真诚友好，坚持互利共赢。两国关系保持健康稳定发展。中方视卢森堡为欧盟内重要合作伙伴，愿同卢方一道努力，建设长期稳定、充满活力的伙伴关系，推动中卢关系不断迈上新台阶"));
+        datas.add(new PokerCardBean(i++, null, "这是图片2", R.drawable.tu2,
+                "双方要发扬优良传统，坚持平等相待、相互尊重，在涉及彼此核心利益和重" +
+                        "大关切问题上相互理解、相互支持，把握两国关系正确发展方向。要提高两国务实合作质量，" +
+                        "扩大优势，挖掘潜力，把金融等传统领域合作做大做强，加快培育航空运输、高新技术、绿色经" +
+                        "济等新的合作增长点，实现更高水平的互利共赢。要深化双方在“一带一路”建设框架内金融和产" +
+                        "能等合作，中方支持建设郑州－卢森堡“空中丝绸之路”。要加强文化、教育、体育等人文交流，" +
+                        "提高人员往来便利化水平。中方期待卢方在中欧关系中继续发挥积极作用，推动欧盟为中欧合作" +
+                        "深入发展提供更多有利条件。"));
+        datas.add(new PokerCardBean(i++, null, "这是图片3", R.drawable.tu3, "贝泰尔表示，卢中建交45年来，两国经贸、金融、交通、文化等领域交流合作取得丰硕成果。我此次访华，旨在推进两国合作关系深入发展。卢中经济互补性强，卢方愿同中方在互尊互信基础上，互学互鉴，进一步拓展双方金融、投资、科技、旅游、人文等各领域交流合作、密切“一带一路”框架下合作。"));
+        datas.add(new PokerCardBean(i++, null, "这是图片4", R.drawable.tu4, "马上就是618，各大电商平台都开始大促销，使出各种手段打折促销，吸引消费者。当然，有真促销，也有先把价格调上去再打折的假促销。像往年一样，今年也有网友质疑部分电商平台，借着打折的幌子欺骗消费者，实际上价格并没有便宜多少。"));
+        datas.add(new PokerCardBean(i++, null, "这是图片5", R.drawable.tu5, "昨天，一位网友在某匿名社交平台发出了一张截图，吐槽京东上一款笔记本，不仅没有比平时便宜，价格反而上涨了不少。"));
+        datas.add(new PokerCardBean(i++, null, "这是图片6", R.drawable.tu6, "根据该网友提供的聊天记录显示，这款京东自营的笔记本电脑在前一日零点前价格为2600元，但是零点之后价格就上涨到2999，随后开始以2799元的价格进行促销。"));
+        datas.add(new PokerCardBean(i++, null, "这是图片7", R.drawable.tu7, "该网友咨询客服时，客服表示，价格是由京东制定的，她没有预知权限。"));
+        datas.add(new PokerCardBean(i++, null, "这是图片8", R.drawable.tu8, "按照这位京东员工的说法，消费者买东西买贵了，只能怪自己，明明有便宜的时候，你为啥不去买呢？"));
 //        datas.add(new PokerCardBean(i++, null, "警告信息9", R.drawable.tu8));
 //        datas.add(new PokerCardBean(i++, null, "警告信息10", R.drawable.tu8));
 //        datas.add(new PokerCardBean(i++, null, "警告信息11", R.drawable.tu8));
